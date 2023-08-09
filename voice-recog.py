e7934a6c486a3744d886c4c9632bcdcc79666ca6
jason <quintanajason4004@gmail.com>
2023-08-09 01:19:09 -0400

    updated the voice recogniton to use mozilla deepspeech instead of google tts

--- voice-recog.py
+++ voice-recog.py
@@ -1,8 +1,22 @@
+import os
+import numpy as np
+import deepspeech
+import pyaudio  # Required for getting audio from microphone
 import speech_recognition as sr
 from gtts import gTTS
-import os
 
-recognizer = sr.Recognizer()
+# Initialize DeepSpeech model
+model = deepspeech.Model('deepspeech-0.9.3-models.pbmm')
+model.enableExternalScorer('deepspeech-0.9.3-models.scorer')
+
+# Configuration for capturing audio from microphone
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000  # DeepSpeech requires audio to be 16kHz
+CHUNK = 1024
+
+# Initialize PyAudio
+audio = pyaudio.PyAudio()
 
 def speak(text):
     tts = gTTS(text=text, lang='en')
@@ -10,28 +24,33 @@ def speak(text):
     os.system("mpg321 response.mp3")
 
 def process_command(command):
-    # Basic commands
     if "hello" in command:
         return "Hello! I'm BMO!"
     elif "how are you" in command:
         return "I'm doing great! Ready to play?"
+    elif "hey bmo whats up" in command:
+        return "who wants to play video games!"
     # ... add other basic commands here
-
-    # If no basic command is recognized, use GPT (or a placeholder for now)
     return "I'm not sure what you said. Can you try again?"
 
-
 while True:
-    with sr.Microphone(device_index=1) as source:
-        print("Listening...")
-        audio_data = recognizer.listen(source)
-        try:
-            text = recognizer.recognize_google(audio_data)
-            print("You said:", text)
-            
-            response = process_command(text)
-            print("BMO:", response)
-            speak(response)
-            
-        except sr.UnknownValueError:
-            print("Sorry I did not hear your request, Please repeat again.")
+    # Start recording
+    stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+    print("Listening...")
+    frames = []
+
+    for _ in range(0, int(RATE / CHUNK * 3)):  # 3 seconds of audio
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    stream.stop_stream()
+    stream.close()
+
+    # Convert audio to numpy array and recognize using DeepSpeech
+    audio_data = np.frombuffer(b''.join(frames), dtype=np.int16)
+    text = model.stt(audio_data)
+    print("You said:", text)
+
+    response = process_command(text)
+    print("BMO:", response)
+    speak(response)
