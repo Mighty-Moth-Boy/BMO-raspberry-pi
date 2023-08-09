73c775fdc7d1f4143f6499e6f3aa18725fe634ac
jason <quintanajason4004@gmail.com>
2023-08-08 22:48:11 -0400

    first draft of the voice commands

---
+++ voice-recog.py
@@ -0,0 +1,37 @@
+import speech_recognition as sr
+from gtts import gTTS
+import os
+
+recognizer = sr.Recognizer()
+
+def speak(text):
+    tts = gTTS(text=text, lang='en')
+    tts.save("response.mp3")
+    os.system("mpg321 response.mp3")
+
+def process_command(command):
+    # Basic commands
+    if "hello" in command:
+        return "Hello! I'm BMO!"
+    elif "how are you" in command:
+        return "I'm doing great! Ready to play?"
+    # ... add other basic commands here
+
+    # If no basic command is recognized, use GPT (or a placeholder for now)
+    return "I'm not sure what you said. Can you try again?"
+
+
+while True:
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio_data = recognizer.listen(source)
+        try:
+            text = recognizer.recognize_google(audio_data)
+            print("You said:", text)
+            
+            response = process_command(text)
+            print("BMO:", response)
+            speak(response)
+            
+        except sr.UnknownValueError:
+            print("Sorry I did not hear your request, Please repeat again.")
