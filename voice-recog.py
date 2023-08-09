4f9e1d66223bcb12360833eb6872c953401145ba
jason <quintanajason4004@gmail.com>
2023-08-09 02:40:15 -0400

    added fuzzy matching for voice recognition

--- voice-recog.py
+++ voice-recog.py
@@ -4,6 +4,9 @@ import deepspeech
 import pyaudio  # Required for getting audio from microphone
 import speech_recognition as sr
 from gtts import gTTS
+from fuzzywuzzy import fuzz
+import random
+
 
 # Initialize DeepSpeech model
 model_path = "/home/pi/BMO/BMO-raspberry-pi/deepspeech-0.9.3-models.tflite"
@@ -27,14 +30,38 @@ def speak(text):
     os.system("mpg321 response.mp3")
 
 def process_command(command):
-    if "hello" in command:
-        return "Hello! I'm BMO!"
-    elif "how are you" in command:
-        return "I'm doing great! Ready to play?"
-    elif "hey bmo whats up" in command:
-        return "who wants to play video games!"
-    # ... add other basic commands here
-    return "I'm not sure what you said. Can you try again?"
+    # Define a threshold for fuzzy matching. If similarity is above this threshold, it's a match.
+    threshold = 85
+    
+    # Command-response mapping
+    responses = {
+        "hello": ["Hello! I'm BMO!", "Hi! BMO here!", "oh hello, wheres finn and jake??"],
+        "how are you": ["I'm doing great! Ready to play?", "Fantastic! How about you?", "Awesome! Let's have some fun!"],
+        "hey whats up": [ "Not much, just being BMO!", "Just hanging around. How about you?"],
+        "im bored": ["who wants to play video games!"]
+
+# on startup: "Who wants to play video games!", on shutdown: "battery low, shutting down"
+
+        # Guess who's late for their video chat
+        # All right, last night, an electric presence came into my room and said, "BMO, I need your perfect body to host the human incarnation of a baby!"
+        # I think I am dying. But that's okay, BMO always bounces back!
+        # ... add other basic commands here
+    }
+
+    # Loop through the commands and see which one has the highest similarity
+    best_match = None
+    highest_ratio = 0
+    for key in responses.keys():
+        current_ratio = fuzz.partial_ratio(command, key)
+        if current_ratio > highest_ratio:
+            highest_ratio = current_ratio
+            best_match = key
+    
+    # If the highest similarity is above the threshold, return a random response from the matched command
+    if highest_ratio > threshold:
+        return random.choice(responses[best_match])
+    
+    return "What?"
 
 while True:
     # Start recording
@@ -57,3 +84,8 @@ while True:
     response = process_command(text)
     print("BMO:", response)
     speak(response)
+
+
+
+
+
